f7443b5ecc61dfd1dd63ef44f6708757cc64a3b6
Daniel <danielparra93@gmail.com>
2016-06-22 18:21:39 -0400

    Visual setup part3

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
-}
+  var start = $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+  var breakBtn = $('#break');
+  var pauseBtn = $('#pause');
+  var resetBtn = $('#reset');
+
+  start.on('click',startCountdown);
+
+
+});
